f13f4d723f26ecf84f2816508529f37ccdfa6e93
Alex <139545638+AlexAlexandru20@users.noreply.github.com>
2025-01-24 20:13:55 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,9 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "123456789")
     
     # Add SERVER_NAME (adjust to your domain or localhost)
-    SERVER_NAME = '127.0.0.1:5000'  # Change if needed
-    PREFERRED_URL_SCHEME = 'http'   # or 'https' for production
+    SERVER_NAME = os.getenv('RENDER_EXTERNAL_HOSTNAME', '127.0.0.1:5000')
+
+    PREFERRED_URL_SCHEME = 'https'   # or 'https' for production
 
     # PostgreSQL Database Configuration
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgre:zbZrGbulcnJXapG7eIpLXbG13FR83W2z@dpg-cu9n1llds78s739g1dj0-a/appointments_db_hqko")  # Use Render’s DB URL
